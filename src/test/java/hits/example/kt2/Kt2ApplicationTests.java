7e328f9ffd71e536e3ffde45785619b746b94e7a
A0l1i1c3e4 <vas120704@gmail.com>
2026-04-21 13:38:56 +0700

    Empty initialized project

---
+++ src/test/java/hits/example/kt2/Kt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hits.example.kt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
